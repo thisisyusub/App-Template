aec13d3355feb6eeecc0e13fca74201267e169ba
KenanYusubov <kanan.yusubov.1999@gmail.com>
2020-04-03 01:44:14 -0700

    widget replaced

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:our_apps_template/bloc/home_bloc//home_bloc.dart';
+import 'package:our_apps_template/data/model/post.dart';
 import 'package:our_apps_template/data/model/user.dart';
 import 'package:our_apps_template/presentation/shared/app_text_styles.dart';
 import 'package:our_apps_template/presentation/widgets/custom_drawer.dart';
@@ -26,7 +27,7 @@ class HomePage extends StatelessWidget {
           } else if (postState is PostLoadInProgress) {
             return _buildPostLoadInProgressWidget();
           } else if (postState is PostLoadSuccess) {
-            return _buildPostLoadSuccessWidget(postState);
+            return PostSuccessWidget(postState.posts);
           }
 
           return Container();
@@ -52,11 +53,47 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _buildPostLoadSuccessWidget(PostLoadSuccess postLoadSuccess) {
-    final posts = postLoadSuccess.posts;
+//  Widget _buildPostLoadSuccessWidget(PostLoadSuccess postLoadSuccess) {
+//    final posts = postLoadSuccess.posts;
+//
+//    return Scrollbar(
+//      child: ListView.separated(
+//        itemBuilder: (context, index) => Container(
+//          margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5),
+//          color: Colors.white,
+//          child: ListTile(
+//            contentPadding: const EdgeInsets.all(8.0),
+//            leading: CircleAvatar(
+//              child: Text(posts[index].userId.toString()),
+//            ),
+//            title: Text(posts[index].title),
+//            subtitle: Text(posts[index].body),
+//            trailing: IconButton(
+//              icon: Icon(Icons.favorite_border),
+//              onPressed: () {},
+//            ),
+//            isThreeLine: true,
+//          ),
+//        ),
+//        separatorBuilder: (context, index) => SizedBox(
+//          height: 5,
+//        ),
+//        itemCount: posts.length,
+//      ),
+//    );
+//  }
+}
 
+/// I have replaced builder method with Widget for [Performance gaining]
+class PostSuccessWidget extends StatelessWidget {
+  final List<Post> posts;
+
+  PostSuccessWidget(this.posts);
+
+  @override
+  Widget build(BuildContext context) {
     return Scrollbar(
-      child: ListView.separated(
+      child: ListView.builder(
         itemBuilder: (context, index) => Container(
           margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5),
           color: Colors.white,
@@ -73,9 +110,6 @@ class HomePage extends StatelessWidget {
             ),
             isThreeLine: true,
           ),
-        ),
-        separatorBuilder: (context, index) => SizedBox(
-          height: 5,
         ),
         itemCount: posts.length,
       ),
