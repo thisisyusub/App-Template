d800d084c8689e6cf7e02bc1a9768c5d64a01c83
KenanYusubov <kanan.yusubov.1999@gmail.com>
2020-04-12 14:37:56 +0400

    connectivity service is added

---
+++ lib/data/service/connectivity_service.dart
@@ -0,0 +1,15 @@
+import 'package:connectivity/connectivity.dart';
+
+class ConnectivityService {
+  ConnectivityService._internal();
+
+  Future<bool> isConnected() async {
+    var connectivity = await (Connectivity().checkConnectivity());
+    if (connectivity == ConnectivityResult.mobile ||
+        connectivity == ConnectivityResult.wifi) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
